e4c5d3b7110a16c021b1099231d195d574e4073e
sebastianocalella <sebastianocalella97@gmail.com>
2022-11-30 17:46:47 +0100

    bg squares implement

--- script.js
+++ script.js
@@ -10,11 +10,18 @@ startElement.addEventListener('click', function(){
 function createSquare (){
     const DivElement = document.createElement('div');
     DivElement.classList.add('square');
+    DivElement.addEventListener('click', function(){
+        DivElement.classList.toggle('bg-blue');
+    })
     return DivElement;
 }
+
+
 
 for (let i = 1; i <= 100; i++){
     const newSquare = createSquare();
     newSquare.innerHTML = i;
     gridElement.appendChild(newSquare);
 }
+
+
